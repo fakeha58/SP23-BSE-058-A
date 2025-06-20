af43718d6154eb9415827bd44227287eab7e5127
fakeha58 <sp23-bse-058@cuilahore.edu.pk>
2025-06-20 09:06:21 +0500

    Delete assignment-3 directory

--- assignment-3/middleware/admin.js
+++
@@ -1,5 +0,0 @@
-module.exports = function(req, res, next) {
-  if (req.session.user && req.session.user.isAdmin) return next();
-  req.flash('error_msg', 'Admins only');
-  res.redirect('/auth/login');
-};
